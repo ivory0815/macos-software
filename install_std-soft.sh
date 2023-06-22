9b4b2753b631ac8dff613d6a280481cbc8deaea9
ivory0815 <42381640+ivory0815@users.noreply.github.com>
2023-06-22 23:39:21 +0200

    Update install_std-soft.sh

--- install_std-soft.sh
+++ install_std-soft.sh
@@ -23,7 +23,7 @@ programs=(
   "displaylink"
   "spotify"
   "handbrake"
-  "cyberghost"
+  "cyberghost-vpn"
   "microsoft-edge"
 )
 
@@ -36,20 +36,3 @@ for program in "${programs[@]}"; do
     brew install --cask "$program"
   fi
 done
-
-# Installiere Microsoft Office mit Microsoft Autoupdate (MAU)
-echo "Installiere Microsoft Office..."
-
-# Überprüfen, ob MAU bereits installiert ist
-if check_program_installed "msupdate"; then
-  # Deinstalliere vorhandene Office-Versionen (optional)
-  echo "Deinstalliere vorhandene Office-Versionen..."
-  /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --uninstall
-fi
-
-# Installiere Office mithilfe von MAU
-/Applications/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --install
-
-echo "Microsoft Office wurde erfolgreich installiert."
-
-echo "Alle Installationen abgeschlossen."
