711e04f83251035733863621a5cdbfafbe1a8629
ivory0815 <42381640+ivory0815@users.noreply.github.com>
2023-06-23 10:12:42 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Funktion zur Überprüfung des Programmstatus
+check_program_installed() {
+  command -v "$1" &>/dev/null
+}
+
+# Liste der Programme
+programs=(
+  "shottr"
+  "deepl"
+  "bitwarden"
+  "alfred"
+  "topnotch"
+  "balenaetcher"
+  "visual-studio-code"
+  "caffeine"
+  "hyper"
+  "istat-menus"
+  "discord"
+  "utm"
+  "languagetool"
+  "displaylink"
+  "spotify"
+  "handbrake"
+  "cyberghost-vpn"
+  "microsoft-edge"
+  "parsec"
+)
+
+# Installationsloop für Programme
+for program in "${programs[@]}"; do
+  if check_program_installed "$program"; then
+    echo "$program ist bereits installiert."
+  else
+    echo "Installiere $program..."
+    brew install --cask "$program"
+  fi
+done
