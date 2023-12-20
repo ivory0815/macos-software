078e9fa5d6f753c93cc236fa27632980bb26918c
ivory0815 <42381640+ivory0815@users.noreply.github.com>
2023-12-20 09:27:59 +0100

    Overhaul 2.0

--- install.sh
+++ install.sh
@@ -1,57 +1,72 @@
 #!/bin/bash
 
 # Liste der Programme
-programs=(
- "shottr"
- "deepl"
- "bitwarden"
- "noir"
- "menubarx"
- "musicbar"
- "alfred"
- "topnotch"
- "balenaetcher"
- "visual-studio-code"
- "caffeine"
- "hyper"
- "istat-menus"
- "discord"
- "utm"
- "languagetool"
- "displaylink"
- "spotify"
- "handbrake"
- "cyberghost-vpn"
- "microsoft-edge"
- "parsec"
- "tinkertool"
+declare -a programs=(
+  "shottr"
+  "deepl"
+  "bitwarden"
+  "menubarx"
+  "alfred"
+  "topnotch"
+  "balenaetcher"
+  "visual-studio-code"
+  "caffeine"
+  "hyper"
+  "istat-menus"
+  "discord"
+  "utm"
+  "languagetool"
+  "displaylink"
+  "handbrake"
+  "microsoft-edge"
+  "parsec"
 )
 
 # Funktion zur Überprüfung des Programmstatus
 check_program_installed() {
-    brew list --cask "$1" &>/dev/null
+  local program="$1"
+  local installed=$(brew list --cask --versions 2>/dev/null | grep "^$program\$")
+
+  if [ -n "$installed" ]; then
+    echo "$program ist als Cask installiert."
+    return 0
+  elif [ -x "$(command -v "$program")" ]; then
+    echo "$program ist als AppImage oder andere Distributionsform installiert."
+    return 0
+  elif [ -d "/Applications/$program.app" ]; then
+    echo "$program ist im Verzeichnis /Applications installiert."
+    return 0
+  else
+    echo "$program ist nicht installiert."
+    return 1
+  fi
 }
 
-# Funktion zur Überprüfung der Programmversion
-check_program_version() {
- program="$1"
- version=$(brew outdated "$program" | grep "^$program$" | cut -d " " -f 2)
- if [ -n "$version" ]; then
-  echo "$program ist in Version $version installiert."
- else
-  echo "$program ist nicht installiert."
- fi
-}
-
-# Überprüfung der Programmversionen
+# Überprüfung der Programme
 for program in "${programs[@]}"; do
- check_program_version "$program"
+  check_program_installed "$program"
 done
 
 # Installation der Programme
-for program in "${programs[@]}"; do
- if ! check_program_installed "$program"; then
-  echo "Installiere $program..."
-  brew install --cask "$program"
- fi
+while true; do
+  read -p "Möchtest du die Programme installieren? (j/n) " answer
+  case "$answer" in
+    j)
+      for program in "${programs[@]}"; do
+        if ! check_program_installed "$program"; then
+          echo "Installiere $program..."
+          if ! brew install --cask "$program"; then
+            echo "Fehler beim Installieren von $program."
+          fi
+        fi
+      done
+      break
+    ;;
+    n)
+      echo "Programme nicht installiert."
+      break
+    ;;
+    *)
+      echo "Ungültige Eingabe. Bitte antworte mit 'j' für Ja oder 'n' für Nein."
+  esac
 done
